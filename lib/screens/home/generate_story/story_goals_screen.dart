08cec102e4820386621c73245b313bd5a60ceffd
debuggerdev <debuggerdev.dds@gmail.com>
2026-01-12 10:18:37 +0530

    12 jan

---
+++ lib/screens/home/generate_story/story_goals_screen.dart
@@ -0,0 +1,236 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:redstreakapp/core/helper/log_helper.dart';
+import 'package:redstreakapp/core/widgets/app_layout.dart';
+import 'package:redstreakapp/providers/home/home_provider.dart';
+
+import '../../../core/constants/app_color.dart';
+import '../../../core/constants/text_style.dart';
+import '../../../core/utils/custom_loader.dart';
+import '../../../core/widgets/app_button.dart';
+import '../../../core/widgets/app_text.dart';
+import '../../../core/widgets/app_textfiled.dart';
+import '../../../core/widgets/custom_toast.dart';
+import '../../../core/widgets/onboarding_widgets.dart';
+import '../../../providers/home/story_provider.dart';
+import '../../../routes/user_routes.dart';
+
+class StoryGoalsScreen extends StatefulWidget {
+  const StoryGoalsScreen({super.key});
+
+  @override
+  State<StoryGoalsScreen> createState() => _StoryGoalsScreenState();
+}
+
+class _StoryGoalsScreenState extends State<StoryGoalsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return AppLayout(
+      body: SafeArea(
+        child: Consumer<StoryProvider>(
+          builder: (context, provider, child) {
+            if (provider.isGetGoalsLoading) {
+              return Center(child: ApiLoadingIndicator());
+            }
+            final goalsList = provider.goalsList;
+
+            return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25.w, vertical: 20.h),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    spacing: 8.w,
+                    children: [
+                      Expanded(
+                        //todo on boarding steps header
+                        child: OnboardingHeader(
+                          currentStep: 1,
+                          totalSteps: 4,
+                          onBack: () {
+                            if (context.canPop()) {
+                              context.pop();
+                            } else {
+                              context.goNamed(AppRoutes.topicsScreen.name);
+                            }
+                          },
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          provider.getStoryInterest(
+                            onFailed: (error) {
+                              AppToast.error(context, error);
+                            },
+                          );
+                          context.pushNamed(AppRoutes.storyInterestScreen.name);
+                        },
+                        child: Text(
+                          "Skip",
+                          style: AppTextStyles.textStyle16Medium.copyWith(
+                            color: AppColors.yellowColor,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  GestureDetector(
+                    // onTap: () {
+                    //   provider.getGoals(
+                    //     onFailed: (error) {
+                    //       AppToast.error(context, error);
+                    //     },
+                    //   );
+                    // },
+                    child: AppText(
+                      text: "What's Your Goal?",
+                      style: AppTextStyles.sfProDisplayBold(fontSize: 32.sp),
+                    ),
+                  ),
+                  10.h.verticalSpace,
+                  AppText(
+                    text:
+                        "Choose what motivates you most — we'll help you reach it.",
+                    style: AppTextStyles.sfProDisplayMedium(
+                      fontSize: 16.sp,
+                      color: AppColors.black.withValues(alpha: 0.8),
+                    ),
+                  ),
+
+                  18.h.verticalSpace,
+
+                  if (provider.isGetGoalsLoading)
+                    const Expanded(child: ApiLoadingIndicator())
+                  else
+                    Expanded(
+                      child: ListView.separated(
+                        itemCount: provider.goalsList.length + 1,
+                        separatorBuilder: (context, index) => 8.h.verticalSpace,
+                        itemBuilder: (context, index) {
+                          // Custom Goal Field at the bottom
+                          if (index == goalsList.length) {
+                            return Padding(
+                              padding: EdgeInsets.only(bottom: 20.h, top: 1.h),
+                              child: Column(
+                                spacing: 8.h,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  if (provider.isCustomGoalSelected)
+                                    AppTextField(
+                                      controller: provider.goalTitleController,
+                                      hintText: "Add Custom Goal Title...",
+                                    ),
+                                  AppTextField(
+                                    onTap: () {
+                                      if (!provider.isCustomGoalSelected) {
+                                        provider.toggleCustomGoal();
+                                      }
+                                    },
+                                    controller: provider.goalDesController,
+                                    hintText: provider.isCustomGoalSelected
+                                        ? "Add Custom Goal Description..."
+                                        : "Add Custom Goal...",
+                                  ),
+                                ],
+                              ),
+                            );
+                          }
+
+                          final goal = goalsList[index];
+
+                          final isSelected = provider.selectedGoalIds.contains(
+                            goal.id,
+                          );
+
+                          return GestureDetector(
+                            onTap: () {
+                              provider.toggleGoal(goal.id);
+                            },
+                            child: Container(
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 16.w,
+                                vertical: 13.h,
+                              ),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(12.r),
+                                border: Border.all(
+                                  color: isSelected
+                                      ? AppColors.teal
+                                      : AppColors.black.withValues(alpha: 0.15),
+                                  width: 1.w,
+                                ),
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  AppText(
+                                    text: goal.title,
+                                    style: AppTextStyles.sfProDisplayBold(
+                                      fontSize: 14.sp,
+                                      color: AppColors.black,
+                                    ),
+                                  ),
+                                  if (goal.description.isNotEmpty) ...[
+                                    3.h.verticalSpace,
+                                    AppText(
+                                      text: goal.description,
+                                      style: AppTextStyles.sfProDisplayRegular(
+                                        fontSize: 14.sp,
+                                        color: AppColors.black,
+                                      ),
+                                    ),
+                                  ],
+                                ],
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+
+                  AppFilledButton(
+                    text: "Next",
+                    backgroundColor: AppColors.yellowColor,
+                    onTap: () async {
+                      final goalText = provider.goalTitleController.text.trim();
+                      final goalDesText = provider.goalDesController.text
+                          .trim();
+
+                      List<String> ids = [];
+                      List<Map<String, String>> customs = [];
+
+                      if (provider.isCustomGoalSelected &&
+                          provider.goalTitleController.text.trim().isNotEmpty) {
+                        customs.add({
+                          "title": provider.goalTitleController.text.trim(),
+                          "description": provider.goalDesController.text.trim(),
+                        });
+                      }
+                      Logger.info(provider.goalsList[0].id);
+
+                      if (goalText.isNotEmpty) {
+                        customs.add({
+                          "title": goalText,
+                          "description": goalDesText,
+                        });
+                      }
+                      provider.customInterestsList.clear();
+                      provider.selectedInterestIds.clear();
+                      if (context.mounted) {
+                        context.pushNamed(AppRoutes.storyInterestScreen.name);
+                      }
+                    },
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/home/generate_story/story_toics_screen.dart
@@ -0,0 +1,270 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:redstreakapp/core/constants/app_assets.dart';
+import 'package:redstreakapp/core/constants/app_color.dart';
+import 'package:redstreakapp/core/constants/text_style.dart';
+import 'package:redstreakapp/core/utils/custom_loader.dart';
+import 'package:redstreakapp/core/utils/de_bouncing.dart';
+import 'package:redstreakapp/core/widgets/app_button.dart';
+import 'package:redstreakapp/core/widgets/app_layout.dart';
+import 'package:redstreakapp/core/widgets/app_text.dart';
+import 'package:redstreakapp/core/widgets/app_textfiled.dart';
+import 'package:redstreakapp/core/widgets/custom_toast.dart';
+import 'package:redstreakapp/core/widgets/onboarding_widgets.dart';
+import 'package:redstreakapp/providers/home/story_provider.dart';
+import 'package:redstreakapp/routes/user_routes.dart';
+import 'package:shimmer/shimmer.dart';
+
+class StoryTopicsScreen extends StatefulWidget {
+  const StoryTopicsScreen({super.key});
+
+  @override
+  State<StoryTopicsScreen> createState() => _StoryTopicsScreenState();
+}
+
+class _StoryTopicsScreenState extends State<StoryTopicsScreen> {
+  String _getIconForTopic(String title) {
+    String lower = title.toLowerCase();
+
+    // Direct Mappings
+    if (lower.contains("space") || lower.contains("planet")) {
+      return AppAssets.space;
+    }
+    if (lower.contains("invention") ||
+        lower.contains("tech") ||
+        lower.contains("science")) {
+      return AppAssets.inventions;
+    }
+    if (lower.contains("haunted") ||
+        lower.contains("ghost") ||
+        lower.contains("horror")) {
+      return AppAssets.hauntedhouse;
+    }
+    if (lower.contains("mystery") ||
+        lower.contains("detective") ||
+        lower.contains("clue")) {
+      return AppAssets.detativeclue;
+    }
+    if (lower.contains("wizard") ||
+        lower.contains("magic") ||
+        lower.contains("fantasy")) {
+      return AppAssets.wizard;
+    }
+    if (lower.contains("dragon") ||
+        lower.contains("animal") ||
+        lower.contains("creature") ||
+        lower.contains("nature")) {
+      return AppAssets.dargon;
+    }
+
+    // Looser Mappings for other known categories
+    if (lower.contains("comic") || lower.contains("fun")) {
+      return AppAssets.wizard;
+    }
+    if (lower.contains("history")) return AppAssets.hauntedhouse;
+    if (lower.contains("adventure")) return AppAssets.space;
+
+    // Rotation fallback for any other topics to ensure variety
+    List<String> fallbacks = [
+      AppAssets.space,
+      AppAssets.inventions,
+      AppAssets.hauntedhouse,
+      AppAssets.detativeclue,
+      AppAssets.wizard,
+      AppAssets.dargon,
+    ];
+    // Use hash code to consistently return the same random image for the same title
+    return fallbacks[title.hashCode.abs() % fallbacks.length];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AppLayout(
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Consumer<StoryProvider>(
+          builder: (context, provider, child) {
+            // Logger.info(
+            //   "Topics List length : ${provider.topicsList[0].interestIds.length}",
+            // );
+            if (provider.isGetTopicsLoading) {
+              return Center(child: ApiLoadingIndicator());
+            }
+            return Padding(
+              padding: EdgeInsets.symmetric(horizontal: 24.w, vertical: 20.h),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  //todo progress step
+                  OnboardingHeader(
+                    currentStep: 3,
+                    totalSteps: 4,
+                    onBack: () {
+                      if (context.canPop()) {
+                        context.pop();
+                      } else {
+                        context.goNamed(AppRoutes.interestsScreen.name);
+                      }
+                    },
+                  ),
+                  AppText(
+                    text: "Choose Your Favorite Topics",
+                    style: AppTextStyles.sfProDisplayBold(
+                      height: 1.2,
+                      fontSize: 32.sp,
+                    ),
+                  ),
+                  10.h.verticalSpace,
+                  AppText(
+                    text:
+                        "Here are some topics we think you'll love based on your interests. You can pick the ones that excite you the most!",
+                    style: AppTextStyles.sfProDisplayMedium(
+                      fontSize: 16.sp,
+                      color: AppColors.black.withValues(alpha: 0.8),
+                    ),
+                  ),
+
+                  //todo Custom Topic Input
+                  18.h.verticalSpace,
+                  AppTextField(
+                    controller: provider.searchTopicCtr,
+                    hintText: "Search Topic...",
+                    onChanged: (value) {
+                      deBouncer.run(() {
+                        provider.getSearchedStoryTopics(
+                          onFailed: (error) {
+                            AppToast.error(context, error);
+                          },
+                        );
+                      });
+                    },
+                  ),
+                  34.h.verticalSpace,
+                  if (provider.searchTopicCtr.text.trim().isEmpty &&
+                      !provider.isGetSearchedTopicsLoading)
+                    Expanded(
+                      child: (provider.isGetTopicsLoading)
+                          ? Center(child: ApiLoadingIndicator())
+                          : GridView(
+                              padding: EdgeInsets.zero,
+                              gridDelegate:
+                                  SliverGridDelegateWithFixedCrossAxisCount(
+                                    crossAxisCount: 2,
+                                    crossAxisSpacing: 20.w,
+                                    mainAxisSpacing: 25.h,
+                                    childAspectRatio: 1.4,
+                                  ),
+                              children: [
+                                //todo  API Topics
+                                ...provider.topicsList.map((topic) {
+                                  final id = topic.id;
+                                  final title = topic.title;
+                                  return TopicCard(
+                                    label: title,
+                                    assetPath: _getIconForTopic(title),
+                                    isSelected: provider.selectedTopicIds
+                                        .contains(id),
+                                    onTap: () => provider.toggleApiTopic(id),
+                                  );
+                                }),
+                              ],
+                            ),
+                    )
+                  else if (provider.isGetSearchedTopicsLoading)
+                    Expanded(child: _shimmer())
+                  else if (provider.searchedTopicsList.isNotEmpty)
+                    Expanded(
+                      child: GridView(
+                        padding: EdgeInsets.zero,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          crossAxisSpacing: 20.w,
+                          mainAxisSpacing: 25.h,
+                          childAspectRatio: 1.4,
+                        ),
+                        children: [
+                          //todo  API Topics
+                          ...provider.searchedTopicsList.map((topic) {
+                            final id = topic.id;
+                            final title = topic.title;
+                            return TopicCard(
+                              label: title,
+                              assetPath: _getIconForTopic(title),
+                              isSelected: provider.selectedTopicIds.contains(
+                                id,
+                              ),
+                              onTap: () => provider.toggleApiTopic(id),
+                            );
+                          }),
+                        ],
+                      ),
+                    )
+                  else if (provider.searchedTopicsList.isEmpty &&
+                      provider.searchTopicCtr.text.trim().isNotEmpty)
+                    Expanded(
+                      child: Padding(
+                        padding: EdgeInsetsGeometry.only(top: 50.h),
+                        child: Align(
+                          alignment: AlignmentGeometry.topCenter,
+                          child: Text("No Topics Found!"),
+                        ),
+                      ),
+                    ),
+
+                  //todo next button
+                  AppFilledButton(
+                    text: "Next",
+                    backgroundColor: AppColors.yellowColor,
+                    margin: EdgeInsetsGeometry.only(top: 4.3.h),
+                    onTap: () async {
+                      if (provider.selectedTopicIds.isEmpty) {
+                        AppToast.error(
+                          context,
+                          "Please select at least one topic",
+                        );
+                        return;
+                      }
+
+                      // final success = await provider.saveTopics(
+                      //   context,
+                      //   topicIds: selectedTopicIds.toList(),
+                      //   customTopics: selectedCustomTopics.toList(),
+                      // );
+                    },
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget _shimmer() {
+    return Shimmer.fromColors(
+      baseColor: const Color(0xFFE5E7EB),
+      highlightColor: const Color(0xFFF3F4F6),
+      child: GridView.builder(
+        physics: const NeverScrollableScrollPhysics(),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 20.w,
+          mainAxisSpacing: 25.h,
+          childAspectRatio: 1.4,
+        ),
+        itemCount: 6, // ✅ 6 shimmer items
+        itemBuilder: (context, index) {
+          return Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16.r),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
